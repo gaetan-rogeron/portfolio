f35ae547d62da5654cfc0bf7f1592a661cd035b7
G R <gaetan.dev@pm.me>
2025-07-18 18:48:31 +0200

    security header

--- next.config.ts
+++ next.config.ts
@@ -10,9 +10,11 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com https://vercel.live;
   style-src 'self' 'unsafe-inline';
   img-src 'self' data: https:;
-  font-src 'self';
-  connect-src 'self' https://vercel-insights.com https://va.vercel-scripts.com https://vercel.live;
+  font-src 'self' data: https:;
+  connect-src 'self' https://va.vercel-scripts.com https://vercel.live;
   frame-ancestors 'none';
+  object-src 'none';
+  base-uri 'self';
 `
   .replace(/\s{2,}/g, " ")
   .trim();
@@ -23,16 +25,24 @@ const securityHeaders = [
     value: ContentSecurityPolicy,
   },
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
   },
   {
     key: "X-Frame-Options",
     value: "DENY",
   },
   {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
     key: "Referrer-Policy",
     value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
   },
 ];
 
